b4e578b7ecc11fda0f9497d827c958f58716e515
James Gibbel <jamesgibbel@gmail.com>
2019-08-08 04:00:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 def roll_call_dwarves(arr)
   # Your code here
-  arr.each.with_index {| a, i |
-    puts "#{i+1} #{a}" }
+  arr.each.with_index {| a, i | puts "#{i+1} #{a}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
+  arr.map { |a| a.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
